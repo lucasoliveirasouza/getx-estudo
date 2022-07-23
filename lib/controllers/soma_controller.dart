1b8e5920de577312316590942ead721772ba6796
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-22 22:26:17 -0300

    Iniciando getx

--- lib/controllers/soma_controller.dart
+++ lib/controllers/soma_controller.dart
@@ -1,13 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SomaController extends GetxController {
   var books = 0.obs;
+  var pens = 0.obs;
 
   increment() {
     books.value++;
   }
 
   decrement() {
-    books.value--;
+    if (books.value <= 0) {
+      Get.snackbar(
+        "Buying Books",
+        "Can not be less than zero",
+        icon: Icon(Icons.alarm),
+        barBlur: 20,
+        isDismissible: true,
+        duration: Duration(
+          seconds: 3,
+        ),
+      );
+    } else {
+      books.value--;
+    }
+  }
+
+  incrementPens() {
+    pens.value++;
+  }
+
+  decrementPens() {
+    if (books.value <= 0) {
+      Get.snackbar(
+        "Buying Pens",
+        "Can not be less than zero",
+        icon: Icon(Icons.alarm),
+        barBlur: 20,
+        isDismissible: true,
+        duration: Duration(
+          seconds: 3,
+        ),
+      );
+    } else {
+      pens.value--;
+    }
   }
 }
