b15e78c9e8605ef68664f3bb73e8113b8c7ea982
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-22 22:04:48 -0300

    Criando interface

--- lib/home.dart
+++ lib/home.dart
@@ -5,24 +5,62 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(30),
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              Text(
-                "Books",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.amber,
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Books",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.amber,
+                  ),
                 ),
-              )
-            ],
-          )
-        ],
+                Expanded(
+                  child: Container(),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.redAccent,
+                  ),
+                  child: IconButton(
+                    onPressed: (){},
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  )
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.redAccent,
+                    ),
+                    child: IconButton(
+                      onPressed: (){},
+                      icon: Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                      ),
+                    )
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
