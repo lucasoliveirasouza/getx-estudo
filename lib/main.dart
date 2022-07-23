25c0359d2e6b6a7580cf474b856a3b2c59d08bc5
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-22 22:57:17 -0300

    Utilizando getx para gerenciar estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:somagetx/controllers/soma_controller.dart';
 import 'package:somagetx/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final SomaController c = Get.put(SomaController());
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
