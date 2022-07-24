3eb264e3e01e39d3a201a3c5c8a644d6b52b4f4f
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-07-23 22:45:04 -0300

    Arrumando interface

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:somagetx/controllers/sum_controller.dart';
+import 'package:somagetx/views/total_page.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({Key? key}) : super(key: key);
@@ -24,167 +25,169 @@ class HomeView extends StatelessWidget {
                 "GetX Flutter",
                 style: TextStyle(
                   fontSize: 40,
+                  color: Colors.white,
                 ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Books",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.amber,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.redAccent,
+                        ),
+                        child: IconButton(
+                          onPressed: () => c.increment(),
+                          icon: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Obx(() => Text(
+                            "${c.books.toString()}",
+                            style: TextStyle(fontSize: 30),
+                          )),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: Colors.redAccent,
+                          ),
+                          child: IconButton(
+                            onPressed: () => c.decrement(),
+                            icon: Icon(
+                              Icons.remove,
+                              color: Colors.white,
+                            ),
+                          )),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Pens",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.amber,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.redAccent,
+                        ),
+                        child: IconButton(
+                          onPressed: () => c.incrementPens(),
+                          icon: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Obx(
+                        () => Text(
+                          "${c.pens.toString()}",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: Colors.redAccent,
+                          ),
+                          child: IconButton(
+                            onPressed: () => c.decrementPens(),
+                            icon: Icon(
+                              Icons.remove,
+                              color: Colors.white,
+                            ),
+                          )),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Container(
+                        width: 150,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.blue.shade900,
+                        ),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            primary: Colors.deepPurple.shade200,
+                          ),
+                          onPressed: () => Get.to(() => TotalPage()),
+                          child: Text(
+                            "Total",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
         ],
       ),
-
-      /*Container(
-        padding: const EdgeInsets.all(30),
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Text(
-                  "Books",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.amber,
-                  ),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: Colors.redAccent,
-                  ),
-                  child: IconButton(
-                    onPressed: () => c.increment(),
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Obx(() => Text(
-                      "${c.books.toString()}",
-                      style: TextStyle(fontSize: 30),
-                    )),
-                SizedBox(
-                  width: 20,
-                ),
-                Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                      color: Colors.redAccent,
-                    ),
-                    child: IconButton(
-                      onPressed: () => c.decrement(),
-                      icon: Icon(
-                        Icons.remove,
-                        color: Colors.white,
-                      ),
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: [
-                Text(
-                  "Pens",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.amber,
-                  ),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: Colors.redAccent,
-                  ),
-                  child: IconButton(
-                    onPressed: () => c.incrementPens(),
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Obx(
-                  () => Text(
-                    "${c.pens.toString()}",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                      color: Colors.redAccent,
-                    ),
-                    child: IconButton(
-                      onPressed: () => c.decrementPens(),
-                      icon: Icon(
-                        Icons.remove,
-                        color: Colors.white,
-                      ),
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(),
-                ),
-                Container(
-                  width: 150,
-                  height: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.blue.shade900,
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      primary: Colors.deepPurple.shade200,
-                    ),
-                    onPressed: () => Get.to(() => TotalPage()),
-                    child: Text(
-                      "Total",
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),*/
     );
   }
 }
